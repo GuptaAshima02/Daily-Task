35c06a4a6150233568329c85ad544ec952d44a5d
Ashima <guptaashima30@gmail.com>
2023-04-24 17:58:13 +0530

    types and operators

--- 05/demos/before/js-demo/home.js
+++ 05/demos/before/js-demo/home.js
@@ -1,7 +1,50 @@
+// let price = 25;
 
+// showMessage(typeof price);//gives us number as a type
 
+// let price = 12;
+// price = price + 1;//addition operator-increment the price by 1
+// price = price - 1;
+// price = price * 1;
+// price = price / 1;
+// price = price % 1;
+//also write as
+// price += 5;
+// price -= 5;
+// price *= 5;
+// price /= 5;
+// price %= 5;
+// showMessage(price) 
 
-let price = 25;
+//working with multiple variables
 
-showMessage(price);
-console.log(price);
+// let price = 12,taxRate = 0.07;
+// showMessage(price * taxRate);
+
+let price = 12;
+showMessage(price) 
+
+// let price = 12;
+// showMessage(++price) --13
+// console.log(++price)
+
+// let price = 12;
+// showMessage(price++) --12
+
+// let price = 12;
+// showMessage(price--) --12
+
+// let price = 12;
+// showMessage(--price) --11
+
+// Strings
+// let message = 'hello World';
+// showMessage(message);
+//for showing ** in output
+// let message = 'hello \"World\"';
+// showMessage(message);
+
+//we can do intrpolation by using backticks[``]
+let name = `Ash`
+let message = `Hello ${name}`;//${variable name} for calling name
+showMessage(message);
